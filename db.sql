9a08976357f2fec073c9917c1e7557860a1c54e1
HumphreyJ <joshhumphrey1@gmail.com>
2020-09-15 14:44:57 -0500

    fixed errors preventing CI I think

--- db.sql
+++ db.sql
@@ -65,3 +65,39 @@ SELECT * FROM restaurants LEFT JOIN (select restaurant_id, COUNT(*), TRUNC(AVG(r
 
 *GET ONE RESTAURANT AND RATINGS
 SELECT * FROM restaurants LEFT JOIN (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews GROUP BY restaurant_id) reviews on restaurants.id = reviews.restaurant_id WHERE restaurant_id = 12;
+
+
+$ npx heroku config:set AWS_ACCESS_KEY_ID = AKIAIDPXXUKRF6ISK3QA AWS_SECRET_ACCES
+S_KEY=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
+
+npx heroku config:set AWS_ACCESS_KEY_ID=AKIAIDPXXUKRF6ISK3QA AWS_SECRET_ACCESS_KEY=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 -a walp
+npx heroku config:set S3_BUCKET_NAME=walp-assets
+
+https://walp.s3.east.amazonaws.com/dump.sql
+
+npx aws s3 presign s3://walp.s3.us-east.amazonaws.com/dump.sql
+
+https://s3.amazonaws.com/walp.s3.us-east-1.amazonaws.com/dump.sql?AWSAccessKeyId=AKIAWERZV52ZPG4TUZEC&Signature=cyk0E0morNquc%2F2NuYwd%2BKorUko%3D&Expires=1600201100
+
+#database URL
+postgres://zlqsgazaesmcqx:eedb6e9d12a18e4615d7ed6c3314ebe43c99b2329cfcd1dfb3379bb0ad0734ec@ec2-52-72-34-184.compute-1.amazonaws.com:5432/d17qagi02pp83v
+
+npx heroku pg:backups:restore "https://s3.amazonaws.com/walp.s3.us-east.amazonaws.com/dump.sql" DATABASE_URL -a walp
+
+#sample AWS bucket policy
+
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1405592139000",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": [
+                "arn:aws:s3:::walpe/*",
+                "arn:aws:s3:::walpe"
+            ]
+        }
+    ]
+}
